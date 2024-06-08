30cb48c2bc3cba43097a337605923a3db360600a
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2024-06-08 11:52:06 +0200

    shuffle order

--- src/main/java/org.contractlib/factory/Commands.java
+++ src/main/java/org.contractlib/factory/Commands.java
@@ -7,15 +7,15 @@ import java.util.List;
 public interface Commands<Term, Type, Datatype, Command> {
     Types<Type> types(List<String> params);
 
+    Datatypes<Type, Datatype> datatypes(List<Pair<String, Integer>> arities);
+
+    Terms<Term, Type> terms(List<Pair<String, Type>> variables);
+
     Command declareSort(String name, Integer arity);
 
     Command defineSort(String name, List<String> params, Type body);
 
-    Datatypes<Type, Datatype> datatypes(List<Pair<String, Integer>> arities);
-
     Command declareDatatypes(List<Pair<String, Integer>> arities, List<Datatype> datatypes);
-
-    Terms<Term, Type> terms(List<Pair<String, Type>> variables);
 
     Command declareFun(String name, List<String> params, List<Type> arguments, Type result);
 
